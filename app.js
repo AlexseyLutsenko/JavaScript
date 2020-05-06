385ecac56449246b78a7991a66b8d8b408b82ea1
Lutsenko Alexsey <mr.loxsis@yandex.ru>
2020-05-06 15:48:10 +0700

    create learn JavaScript

---
+++ app.js
@@ -0,0 +1,66 @@
+window.onload = function app() {
+
+//Построение треугольника в цикле
+// let str = '';
+//
+// for (let i = 0; i < 7; i++) {
+//
+//     console.log(str += '#');
+//
+// }
+
+//FizzBuzz
+// let result;
+// for (let i = 1; i <= 100; i++) {
+//
+//     if(i % 3 == 0 && i % 5 == 0) {
+//         result = 'FizzBuzz'
+//     } else if (i % 3 == 0) {
+//         result = 'Fizz'
+//     } else if (i % 5 == 0) {
+//         result = 'Buzz'
+//     } else  {
+//         result = i
+//     }
+//
+//     console.log(result);
+//
+// }
+
+//Шахматная доска
+// let width = 10;
+// let height = 8;
+// let result ='';
+//
+// for (let i = 0; i < height; i++) {
+//     for (let a = 0; a < width; a++) {
+//
+//         if (i % 2 == 0) {
+//             if (a % 2 == 0) {
+//                 result += ' ';
+//             } else {
+//                 result += '#';
+//             }
+//         } else {
+//             if (a % 2 == 0) {
+//                 result += '#';
+//             } else {
+//                 result += ' ';
+//             }
+//         }
+//     }
+//
+//     result += '\n';
+// }
+//
+// console.log(result);
+
+
+
+
+
+
+
+};
+
+
